d870ef3710a96ddd0d7611f8d7efb69d140c9b3e
Frehiwot702 <frehiwot.tewodros112@gmail.com>
2026-02-27 10:53:42 -0600

    fix/ update socket URL to deployed backend

--- app/socket.ts
+++ app/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:5000";
+const SOCKET_URL = "https://syncspace-server-jfmb.onrender.com";
 
 export const socket: Socket = io(SOCKET_URL, {
   autoConnect: false
